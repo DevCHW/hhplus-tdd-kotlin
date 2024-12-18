fcbd6a7df3ab596e27a367bd1a2900f11c02b963
DevCHW <ggoma003@naver.com>
2024-12-19 05:51:33 +0900

    [feat] : 유저 포인트 생성 정책 추가

--- src/main/kotlin/io/hhplus/tdd/point/UserPoint.kt
+++ src/main/kotlin/io/hhplus/tdd/point/UserPoint.kt
@@ -4,4 +4,21 @@ data class UserPoint(
     val id: Long,
     val point: Long,
     val updateMillis: Long,
-)
+) {
+
+    init {
+        require(point >= MIN_POINT) {
+            throw IllegalArgumentException("유저 포인트는 ${MIN_POINT} 미만일 수 없습니다.")
+        }
+
+        require(point <= MAX_POINT) {
+            throw IllegalArgumentException("유저 포인트는 ${MAX_POINT} 포인트를 초과할 수 없습니다.")
+        }
+    }
+
+    companion object {
+        const val MAX_POINT = 1_000_000L
+        const val MIN_POINT = 0L
+    }
+
+}
